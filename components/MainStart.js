c723313d7545582cfd232c23d15c884ff42c9045
Michael King <michaelking.mgk@gmail.com>
2022-11-13 11:40:34 +0800

    front end commit.

--- components/MainStart.js
+++ components/MainStart.js
@@ -2,12 +2,12 @@ import Head from "next/head";
 import Header from "../components/Header";
 import PlayerCounter from "../components/PlayerCounter";
 import {
-    mainAddresses,
-    controlsAddresses,
-    dataAddresses,
-    mainABI,
-    controlsABI,
-    dataABI,
+  mainAddresses,
+  controlsAddresses,
+  dataAddresses,
+  mainABI,
+  controlsABI,
+  dataABI,
 } from "../constants";
 // dont export from moralis when using react
 import { useMoralis, useWeb3Contract } from "react-moralis";
@@ -17,143 +17,137 @@ import { ethers } from "ethers";
 const supportedChains = ["5", "31337"];
 
 export default function MainStart() {
-    const { Moralis, isWeb3Enabled, account, chainId: chainIdHex } = useMoralis();
-    // These get re-rendered every time due to our connect button!
-    const chainId = parseInt(chainIdHex);
-    console.log(`ChainId is ${chainId}`);
-    const mainAddress = chainId in mainAddresses ? mainAddresses[chainId][0] : null;
+  const { Moralis, isWeb3Enabled, account, chainId: chainIdHex } = useMoralis();
+  // These get re-rendered every time due to our connect button!
+  const chainId = parseInt(chainIdHex);
+  console.log(`ChainId is ${chainId}`);
+  const mainAddress =
+    chainId in mainAddresses ? mainAddresses[chainId][0] : null;
 
-    // State hooks
-    // https://stackoverflow.com/questions/58252454/react-hooks-using-usestate-vs-just-variables
-    const [entranceFee, setEntranceFee] = useState("0");
-    const [numberOfPlayers, setNumberOfPlayers] = useState("0");
-    const [lobbyFull, setLobbyFull] = useState(false);
+  // State hooks
+  // https://stackoverflow.com/questions/58252454/react-hooks-using-usestate-vs-just-variables
+  const [entranceFee, setEntranceFee] = useState("0");
+  const [numberOfPlayers, setNumberOfPlayers] = useState("0");
+  const [lobbyFull, setLobbyFull] = useState(false);
 
-    const dispatch = useNotification();
+  const dispatch = useNotification();
 
-    const {
-        runContractFunction: enterLobby,
-        isLoading,
-        isFetching,
-    } = useWeb3Contract({
-        abi: mainABI,
-        contractAddress: mainAddress,
-        functionName: "enterLobby",
-        params: {},
-        msgValue: entranceFee,
-        onError: (error) => console.log(error),
+  const {
+    runContractFunction: enterLobby,
+    isLoading,
+    isFetching,
+  } = useWeb3Contract({
+    abi: mainABI,
+    contractAddress: mainAddress,
+    functionName: "enterLobby",
+    params: {},
+    msgValue: entranceFee,
+    onError: (error) => console.log(error),
+  });
+
+  /* View Functions */
+
+  const { runContractFunction: getEntranceFee } = useWeb3Contract({
+    abi: mainABI,
+    contractAddress: mainAddress, // specify the networkId
+    functionName: "getEntranceFee",
+    params: {},
+    onError: (error) => console.log(error),
+  });
+
+  const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
+    abi: mainABI,
+    contractAddress: mainAddress,
+    functionName: "getNumberOfPlayers",
+    params: {},
+    onError: (error) => console.log(error),
+  });
+
+  async function updateUIValues() {
+    const entranceFeeFromCall = (await getEntranceFee()).toString();
+    setEntranceFee(entranceFeeFromCall);
+  }
+
+  const handleNewNotification = () => {
+    dispatch({
+      type: "info",
+      message: "Transaction Complete!",
+      title: "Transaction Notification",
+      position: "topR",
+      icon: "bell",
     });
+  };
 
-    /* View Functions */
-
-    const { runContractFunction: getEntranceFee } = useWeb3Contract({
-        abi: mainABI,
-        contractAddress: mainAddress, // specify the networkId
-        functionName: "getEntranceFee",
-        params: {},
-        onError: (error) => console.log(error),
-    });
-
-    const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
-        abi: mainABI,
-        contractAddress: mainAddress,
-        functionName: "getNumberOfPlayers",
-        params: {},
-        onError: (error) => console.log(error),
-    });
-
-    async function updateUIValues() {
-        const entranceFeeFromCall = (await getEntranceFee()).toString();
-        setEntranceFee(entranceFeeFromCall);
+  const handleSuccess = async (tx) => {
+    try {
+      await tx.wait(1);
+      handleNewNotification(tx);
+      const numberOfPlayers = (await getNumberOfPlayers()).toString();
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const handleNewNotification = () => {
-        dispatch({
-            type: "info",
-            message: "Transaction Complete!",
-            title: "Transaction Notification",
-            position: "topR",
-            icon: "bell",
-        });
-    };
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      updateUIValues();
+    }
+  }, [isWeb3Enabled]);
 
-    const handleSuccess = async (tx) => {
-        try {
-            await tx.wait(1);
-            handleNewNotification(tx);
-            const numberOfPlayers = (await getNumberOfPlayers()).toString();
-            if (numberOfPlayers == 4) {
-                setLobbyFull(lobbyFull);
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  useEffect(() => {
+    if (numberOfPlayers < 4) {
+    } else {
+      <div></div>;
+    }
+  });
 
-    useEffect(() => {
-        if (isWeb3Enabled) {
-            updateUIValues();
-            console.log(isFull);
-        }
-    }, [isWeb3Enabled]);
-
-    useEffect(() => {
-        if (numberOfPlayers < 4) {
-        } else {
-            <div></div>;
-        }
-    });
-
-    return (
-        <div>
-            {isWeb3Enabled ? (
-                <div className="flex place-content-center">
-                    {supportedChains.includes(parseInt(chainId).toString()) ? (
-                        <div>
-                            {mainAddress ? (
-                                <>
-                                    <button
-                                        className="bg-blue-500 hover:bg-green-700 text-white font-bold px-4 py-2 align-middle rounded ml-auto"
-                                        onClick={async () =>
-                                            await enterLobby({
-                                                onSuccess: handleSuccess,
-                                                onError: (error) => console.log(error),
-                                            })
-                                        }
-                                        disabled={isLoading || isFetching || isFull}
-                                    >
-                                        {isLoading || isFetching ? (
-                                            <div className="animate-spin spinner-border h-6 w-6 hover:bg-green-700 border-b-2 rounded-full"></div>
-                                        ) : (
-                                            <div>
-                                                Join Player Lobby{" "}
-                                                <div>
-                                                    (Fee: {""}
-                                                    {ethers.utils.formatUnits(entranceFee, "ether")}
-                                                    ETH)
-                                                </div>
-                                            </div>
-                                        )}
-                                        {isFull ? (
-                                            (button.style.visibility = "hidden")
-                                        ) : (
-                                            <div></div>
-                                        )}
-                                    </button>
-                                    <br />
-                                    <br />
-                                </>
-                            ) : (
-                                <div>Please connect to a supported chain </div>
-                            )}
-                        </div>
+  return (
+    <div class="invisible">
+      {isWeb3Enabled ? (
+        <div className="flex place-content-center">
+          {supportedChains.includes(parseInt(chainId).toString()) ? (
+            <div>
+              {mainAddress ? (
+                <>
+                  <button
+                    className="bg-blue-500 hover:bg-green-700 text-white font-bold px-4 py-2 align-middle rounded ml-auto"
+                    onClick={async () =>
+                      await enterLobby({
+                        onSuccess: handleSuccess,
+                        onError: (error) => console.log(error),
+                      })
+                    }
+                    disabled={isLoading || isFetching}
+                  >
+                    {isLoading || isFetching ? (
+                      <div className="animate-spin spinner-border h-6 w-6 hover:bg-green-700 border-b-2 rounded-full"></div>
                     ) : (
-                        <div className="text-white text-center">{`Please switch to a supported chainId. \n The supported Chain Ids are: ${supportedChains}`}</div>
+                      <div>
+                        Join Player Lobby{" "}
+                        <div>
+                          (Fee: {""}
+                          {ethers.utils.formatUnits(entranceFee, "ether")}
+                          ETH)
+                        </div>
+                      </div>
                     )}
-                </div>
-            ) : (
-                <div className="text-white text-center">Please Connect Your Wallet To Continue</div>
-            )}
+                  </button>
+                  <br />
+                  <br />
+                </>
+              ) : (
+                <div>Please connect to a supported chain </div>
+              )}
+            </div>
+          ) : (
+            <div className="text-white text-center">{`Please switch to a supported chainId. \n The supported Chain Ids are: ${supportedChains}`}</div>
+          )}
         </div>
-    );
+      ) : (
+        <div className="text-white text-center">
+          Please Connect Your Wallet To Continue
+        </div>
+      )}
+    </div>
+  );
 }
--- components/PlayerCounter.js
+++ components/PlayerCounter.js
@@ -4,149 +4,157 @@ import { useMoralis, useWeb3Contract } from "react-moralis";
 import { useEffect, useState } from "react";
 import { useNotification } from "@web3uikit/core";
 import { ethers } from "ethers";
-import truncateEthAddress from "truncate-eth-address";
 
 export default function PlayerCounter() {
-    const { Moralis, isWeb3Enabled, chainId: chainIdHex } = useMoralis();
-    // These get re-rendered every time due to our connect button!
-    const chainId = parseInt(chainIdHex);
-    const mainAddress = chainId in mainAddresses ? mainAddresses[chainId][0] : null;
-    const [player1, setPlayer1] = useState("N/A");
-    const [player2, setPlayer2] = useState("N/A");
-    const [player3, setPlayer3] = useState("N/A");
-    const [player4, setPlayer4] = useState("N/A");
-    const [numberOfPlayers, setNumberOfPlayers] = useState("0");
-    const dispatch = useNotification();
+  const { Moralis, isWeb3Enabled, chainId: chainIdHex } = useMoralis();
+  // These get re-rendered every time due to our connect button!
+  const chainId = parseInt(chainIdHex);
+  const mainAddress =
+    chainId in mainAddresses ? mainAddresses[chainId][0] : null;
+  const [player1, setPlayer1] = useState("N/A");
+  const [player2, setPlayer2] = useState("N/A");
+  const [player3, setPlayer3] = useState("N/A");
+  const [player4, setPlayer4] = useState("N/A");
+  const [numberOfPlayers, setNumberOfPlayers] = useState("0");
+  const dispatch = useNotification();
 
-    /* View Functions */
+  /* View Functions */
 
-    const {
-        runContractFunction: getNumberOfPlayers,
-        isLoading,
-        isFetching,
-    } = useWeb3Contract({
-        abi: mainABI,
-        contractAddress: mainAddress,
-        functionName: "getNumberOfPlayers",
-        params: {},
-        onError: (error) => console.log(error),
-    });
-    const { runContractFunction: getPlayer1 } = useWeb3Contract({
-        abi: mainABI,
-        contractAddress: mainAddress,
-        functionName: "getPlayer",
-        params: { index: 0 },
-        onError: (error) => console.log(error),
-    });
-    const { runContractFunction: getPlayer2 } = useWeb3Contract({
-        abi: mainABI,
-        contractAddress: mainAddress,
-        functionName: "getPlayer",
-        params: { index: 1 },
-        onError: (error) => console.log(error),
-    });
-    const { runContractFunction: getPlayer3 } = useWeb3Contract({
-        abi: mainABI,
-        contractAddress: mainAddress,
-        functionName: "getPlayer",
-        params: { index: 2 },
-        onError: (error) => console.log(error),
-    });
-    const { runContractFunction: getPlayer4 } = useWeb3Contract({
-        abi: mainABI,
-        contractAddress: mainAddress,
-        functionName: "getPlayer",
-        params: { index: 3 },
-        onError: (error) => console.log(error),
-    });
+  const {
+    runContractFunction: getNumberOfPlayers,
+    isLoading,
+    isFetching,
+  } = useWeb3Contract({
+    abi: mainABI,
+    contractAddress: mainAddress,
+    functionName: "getNumberOfPlayers",
+    params: {},
+    onError: (error) => console.log(error),
+  });
+  const { runContractFunction: getPlayer1 } = useWeb3Contract({
+    abi: mainABI,
+    contractAddress: mainAddress,
+    functionName: "getPlayer",
+    params: { index: 0 },
+    onError: (error) => console.log(error),
+  });
+  const { runContractFunction: getPlayer2 } = useWeb3Contract({
+    abi: mainABI,
+    contractAddress: mainAddress,
+    functionName: "getPlayer",
+    params: { index: 1 },
+    onError: (error) => console.log(error),
+  });
+  const { runContractFunction: getPlayer3 } = useWeb3Contract({
+    abi: mainABI,
+    contractAddress: mainAddress,
+    functionName: "getPlayer",
+    params: { index: 2 },
+    onError: (error) => console.log(error),
+  });
+  const { runContractFunction: getPlayer4 } = useWeb3Contract({
+    abi: mainABI,
+    contractAddress: mainAddress,
+    functionName: "getPlayer",
+    params: { index: 3 },
+    onError: (error) => console.log(error),
+  });
 
-    async function updatePlayerUI() {
-        const numberOfPlayersFromCall = (await getNumberOfPlayers()).toString();
-        setNumberOfPlayers(numberOfPlayersFromCall);
-        const player1 = await getPlayer1();
-        const player2 = await getPlayer2();
-        const player3 = await getPlayer3();
-        const player4 = await getPlayer4();
-        try {
-            setPlayer1(player1.slice(0, 6) + "..." + player1.slice(-6, player1.length));
-            setPlayer2(player2.slice(0, 6) + "..." + player1.slice(-6, player1.length));
-            setPlayer3(player3.slice(0, 6) + "..." + player1.slice(-6, player1.length));
-            setPlayer4(player4.slice(0, 6) + "..." + player1.slice(-6, player1.length));
-        } catch {
-            console.log("Cant slice all yet");
-        }
+  async function updatePlayerUI() {
+    const numberOfPlayersFromCall = (await getNumberOfPlayers()).toString();
+    setNumberOfPlayers(numberOfPlayersFromCall);
+    const player1 = await getPlayer1();
+    const player2 = await getPlayer2();
+    const player3 = await getPlayer3();
+    const player4 = await getPlayer4();
+    try {
+      setPlayer1(
+        player1.slice(0, 6) + "..." + player1.slice(-6, player1.length)
+      );
+      setPlayer2(
+        player2.slice(0, 6) + "..." + player1.slice(-6, player1.length)
+      );
+      setPlayer3(
+        player3.slice(0, 6) + "..." + player1.slice(-6, player1.length)
+      );
+      setPlayer4(
+        player4.slice(0, 6) + "..." + player1.slice(-6, player1.length)
+      );
+    } catch {
+      console.log("Cant slice all yet");
     }
+  }
 
-    useEffect(() => {
-        if (isWeb3Enabled) {
-            updatePlayerUI();
-        }
-    }, [isWeb3Enabled]);
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      updatePlayerUI();
+    }
+  }, [isWeb3Enabled]);
 
-    return (
-        <div>
-            <div className="pb-2 place-content-center content-center pl-5 pr-3">
-                {mainAddress ? (
-                    <>
-                        <button
-                            className="bg-blue-500 hover:bg-green-700 text-white font-bold place-content-center justify-center px-2 rounded ml-auto"
-                            onClick={async () =>
-                                await getNumberOfPlayers({
-                                    onSuccess: updatePlayerUI,
-                                    onError: (error) => console.log(error),
-                                })
-                            }
-                        >
-                            {isLoading || isFetching ? (
-                                <div className="spinner-border"></div>
-                            ) : (
-                                <div>Refresh Paid Players in Lobby</div>
-                            )}
-                        </button>
-                        ;
-                    </>
-                ) : (
-                    <div className="text-center text-white">Please connect to Web3</div>
-                )}
-            </div>
-            <div className="place-content-center">
-                {mainAddress ? (
-                    <>
-                        <Table
-                            alignCellItems="center"
-                            columnsConfig="120px 1fr"
-                            data={[
-                                [<Tag color="red" theme="chips" text="Player 1" />, player1],
-                                [<Tag color="green" theme="chips" text="Player 2" />, player2],
-                                [<Tag color="yellow" theme="chips" text="Player 3" />, player3],
-                                [<Tag color="blue" theme="chips" text="Player 4" />, player4],
-                            ]}
-                            header={[`PLAYERS: ${numberOfPlayers}`]}
-                            noPagination
-                            onRowClick={function noRefCheck() {}}
-                            tableBackgroundColor="darkgrey"
-                        />
-                        ;
-                    </>
-                ) : (
-                    <div>
-                        <Table
-                            alignCellItems="center"
-                            columnsConfig="120px 1fr"
-                            data={[
-                                [<Tag color="red" theme="chips" text="Player 1" />, player1],
-                                [<Tag color="green" theme="chips" text="Player 2" />, player2],
-                                [<Tag color="yellow" theme="chips" text="Player 3" />, player3],
-                                [<Tag color="blue" theme="chips" text="Player 4" />, player4],
-                            ]}
-                            header={[`PLAYERS: ${numberOfPlayers}`]}
-                            noPagination
-                            onRowClick={function noRefCheck() {}}
-                            tableBackgroundColor="darkgrey"
-                        />
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="pb-2 place-content-center content-center pl-5 pr-3">
+        {mainAddress ? (
+          <>
+            <button
+              className="bg-blue-500 hover:bg-green-700 text-white font-bold place-content-center justify-center px-2 rounded ml-auto"
+              onClick={async () =>
+                await getNumberOfPlayers({
+                  onSuccess: updatePlayerUI,
+                  onError: (error) => console.log(error),
+                })
+              }
+            >
+              {isLoading || isFetching ? (
+                <div className="spinner-border"></div>
+              ) : (
+                <div>Refresh Paid Players in Lobby</div>
+              )}
+            </button>
+            ;
+          </>
+        ) : (
+          <div className="text-center text-white">Please connect to Web3</div>
+        )}
+      </div>
+      <div className="place-content-center">
+        {mainAddress ? (
+          <>
+            <Table
+              alignCellItems="center"
+              columnsConfig="120px 1fr"
+              data={[
+                [<Tag color="blue" theme="chips" text="Player 1" />, player1],
+                [<Tag color="green" theme="chips" text="Player 2" />, player2],
+                [<Tag color="yellow" theme="chips" text="Player 3" />, player3],
+                [<Tag color="blue" theme="chips" text="Player 4" />, player4],
+              ]}
+              header={[`PLAYERS: ${numberOfPlayers}`]}
+              noPagination
+              onRowClick={function noRefCheck() {}}
+              tableBackgroundColor="darkgrey"
+            />
+            ;
+          </>
+        ) : (
+          <div>
+            <Table
+              alignCellItems="center"
+              columnsConfig="120px 1fr"
+              data={[
+                [<Tag color="red" theme="chips" text="Player 1" />, player1],
+                [<Tag color="green" theme="chips" text="Player 2" />, player2],
+                [<Tag color="yellow" theme="chips" text="Player 3" />, player3],
+                [<Tag color="blue" theme="chips" text="Player 4" />, player4],
+              ]}
+              header={[`PLAYERS: ${numberOfPlayers}`]}
+              noPagination
+              onRowClick={function noRefCheck() {}}
+              tableBackgroundColor="darkgrey"
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
